24011115dfebfeabbaa3aa3c1b32d1d22b817969
Franjo Mindek <franjo.mindek@gmail.com>
2023-04-06 16:12:04 +0200

    possible implementation

--- src/app/data/services/codebooks.service.ts
+++ src/app/data/services/codebooks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
-import { Codebooks } from '../types/Codebook';
+import { Observable, ReplaySubject, tap } from 'rxjs';
+import { Codebook, Codebooks } from '../types/Codebook';
 import { ApiService } from '../../core/services/ApiService';
 
 @Injectable({
@@ -8,15 +8,37 @@ import { ApiService } from '../../core/services/ApiService';
 })
 export class CodebooksService {
 
-  private codebooks$: ReplaySubject<Codebooks> = new ReplaySubject<Codebooks>(1);
+  private codebooks$: {[index: string]: ReplaySubject<Codebook[]>} = {};
 
-  constructor(private api: ApiService) {
-    console.log('radim');
-    this.api.get<Codebooks>(`/codebooks`)
-        .subscribe(this.codebooks$);
+  constructor(
+    private api: ApiService,
+  ) {
   }
 
-  public getCodebooks() {
-    return this.codebooks$.asObservable();
+  public getCodebooks(...codebookNames: string[]): [{[p: string]: Observable<Codebook[]>}, Observable<Codebooks> | null] {
+
+    const codebookNamesHere = codebookNames.filter(codebookName => this.codebooks$[codebookName]);
+    const codebookNamesToFetch = codebookNames.filter(codebookName => !this.codebooks$[codebookName]);
+
+    const codebooksHere: {[index: string]: Observable<Codebook[]>} = {};
+    codebookNamesHere.forEach(name => codebooksHere[name] = this.codebooks$[name].asObservable());
+
+    if (codebookNamesToFetch.length > 0) {
+      const fetching = this.api.get<Codebooks>(`/codebooks/${codebookNamesToFetch}`)
+                           .pipe(
+                             tap(codebooks => {
+                               Object.keys(codebooks)
+                                     .forEach(name => {
+                                       const subject = new ReplaySubject<Codebook[]>(1);
+                                       subject.next(codebooks[name]);
+                                       this.codebooks$[name] = subject;
+                                     });
+                             }),
+                           );
+
+      return [codebooksHere, fetching];
+    }
+
+    return [codebooksHere, null];
   }
 }
